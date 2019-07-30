975e5bd0cce2687534ae7a93bd14dbb8c3c2149a
gekkedev <17025257+gekkedev@users.noreply.github.com>
2019-07-30 21:15:36 +0200

    implemented callback index nullability

--- index.ts
+++ index.ts
@@ -8,8 +8,10 @@ interface Array<T> {
   * Performs the specified action for each element in an array synchronously.
   * @param callback  A function that accepts two arguments. ezEach calls the function one time for each element in the array.
   */
-Array.prototype.ezEach = function (callback: (value: any, index: number) => void) { //note to self: using ES6 arrow functions would rip `this` out of it's context
+Array.prototype.ezEach = function (callback: (value: any, index?: number) => void) { //note to self: using ES6 arrow functions would rip `this` out of it's context
   for (let i = 0; i < this.length; i++) {
-    callback(this[i], i);
+    if (typeof(index= == "undefined") {
+      callback(this[i]);
+    } else callback(this[i], i);
   }
 }
